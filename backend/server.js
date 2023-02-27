ac1005ad1471c73c0840ee3668ce7ea050af4b1f
ambili <ambilipakhd@gmail.com>
2023-02-27 11:18:15 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const dotenv=require('dotenv').config()
+const port=process.env.PORT || 5000;
+const connectDB=require('./config/db')
+const app=express()
+
+
+connectDB()
+
+
+const cors = require('cors')
+app.use(cors())
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.get('/',(req,res)=>{
+    res.send('Api running')
+})
+
+
+app.use('/',require('./routes/adminRoutes'))
+app.use('/',require('./routes/employeeRoutes'))
+
+app.listen(port,()=>console.log(`Server started on port ${port}`))
